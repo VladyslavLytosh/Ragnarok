c29a72288e1cddbabc49ae0cc4246416c1a10551
Vladyslav Lytosh <vllitosh46@gmail.com>
2024-01-25 11:59:54 +0200

    [Range] Added range class, linked animation,  fix projectile issue

--- Source/Ragrarok/Private/Animations/RagnarokAnimInstance.cpp
+++ Source/Ragrarok/Private/Animations/RagnarokAnimInstance.cpp
@@ -2,3 +2,25 @@
 
 
 #include "Animations/RagnarokAnimInstance.h"
+
+#include "Characters/BaseCharacter.h"
+
+void URagnarokAnimInstance::OnInit_Implementation()
+{
+	Super::OnInit_Implementation();
+
+	if (ABaseCharacter* Character = Cast<ABaseCharacter>(GetOwningActor()))
+	{
+		CachedCharacter = Character;
+	}
+}
+
+void URagnarokAnimInstance::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (CachedCharacter)
+	{
+		CharacterVelocityLength = CachedCharacter->GetVelocity().Length();
+	}
+}
--- Source/Ragrarok/Public/Animations/RagnarokAnimInstance.h
+++ Source/Ragrarok/Public/Animations/RagnarokAnimInstance.h
@@ -6,6 +6,7 @@
 #include "PaperZDAnimInstance.h"
 #include "RagnarokAnimInstance.generated.h"
 
+class ABaseCharacter;
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FAttackNotifyDelegate);
 
 /**
@@ -17,4 +18,15 @@ class RAGRAROK_API URagnarokAnimInstance : public UPaperZDAnimInstance
 	GENERATED_BODY()
 public:
 	FAttackNotifyDelegate AttackNotifyDelegate;
+	
+protected:
+	virtual void OnInit_Implementation() override;
+	
+	virtual void Tick(float DeltaTime) override;
+	
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	TObjectPtr<ABaseCharacter> CachedCharacter;
+	
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	float CharacterVelocityLength;
 };
