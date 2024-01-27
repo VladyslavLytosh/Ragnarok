c1d4de1d4876205eacfdf8da7364ce76f5e7031b
Ivan Seleznov <ivan14.02434@gmail.com>
2024-01-27 11:01:27 +0200

    [AI] Punk with pistol

--- Source/Ragrarok/Public/Characters/CharacterClassComponent.h
+++ Source/Ragrarok/Public/Characters/CharacterClassComponent.h
@@ -25,6 +25,7 @@ public:
 
 	const TArray<TSubclassOf<UCharacterClass>>& GetAvailableCharacterClasses() const { return AvailableCharacterClasses; }
 
+	UFUNCTION(BlueprintCallable)
 	void SetCurrentCharacterClass(UCharacterClass* InCurrentCharacterClass, ABaseCharacter* Character);
 	
 protected:
